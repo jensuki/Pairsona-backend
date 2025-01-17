69d15a426a47f635034d5aab8e623416a7d7c9f7
jensuki <jeni.suk@proton.me>
2025-01-16 20:21:53 -0800

    removed sensitive logs from config

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ require('colors');
 
 // environmental variables
 const SECRET_KEY = process.env.SECRET_KEY || 's3cr3tk3y';
-const PORT = +process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 // get uri for test or production
 const getDatabaseUri = () => {
@@ -20,11 +20,8 @@ const getDatabaseUri = () => {
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === 'test' ? 1 : 12;
 
 console.log("Pairsona Config:".green);
-console.log("SECRET_KEY:".yellow, SECRET_KEY);
 console.log("PORT:".yellow, PORT.toString());
 console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:".yellow, getDatabaseUri());
-console.log("Test Database URL:".yellow, process.env.DATABASE_URL_TEST);
 console.log("-----------");
 console.log("NODE_ENV:".yellow, process.env.NODE_ENV);
 console.log("Using Database:".yellow, getDatabaseUri());
