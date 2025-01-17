7a417b87f9c27012012cf7caccb115e40f883093
jensuki <jeni.suk@proton.me>
2025-01-16 21:31:13 -0800

    updated server port for render

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
 
 /** starting up server for pairsona */
 
-const { PORT } = require('./config/config');
 const app = require('./app');
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
