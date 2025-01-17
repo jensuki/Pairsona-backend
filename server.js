c61c185ba7616439b61a7851c3c12f365ca3b2cf
jensuki <jeni.suk@proton.me>
2025-01-16 21:40:35 -0800

    fixing port +1

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 /** starting up server for pairsona */
 
 const app = require('./app');
+const port = process.env.PORT || 3001;
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
