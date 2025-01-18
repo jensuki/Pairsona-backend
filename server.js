aebc855d89226031ad2ee4cbc88d41984009b939
jensuki <jeni.suk@proton.me>
2025-01-17 16:32:23 -0800

    +5

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const { PORT } = require('./config/config');
 const app = require('./app');
 
-// debug
-console.log("Environment PORT value:", process.env.PORT);  // check if Render provides PORT
-console.log("Resolved PORT from config:", PORT);           // verify PORT from config.js
-console.log("About to start the server...");
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
