0ca552b92132a447196a60d8e2baea6be267976c
jensuki <jeni.suk@proton.me>
2025-01-17 16:42:43 -0800

    final app.js

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ app.use(authenticateJWT);
 
 // serve static files from build directory
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
-console.log('Static files path:', path.join(__dirname, '../frontend/dist'));
-
 
 // root route
 app.get('/', (req, res) => {
@@ -48,7 +46,6 @@ app.use('/messages', messageRoutes);
 
 // catch all handler for all other routes (for client side navigating)
 app.get('*', (req, res) => {
-    console.log("Serving React app for route:", req.url);
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 })
 
